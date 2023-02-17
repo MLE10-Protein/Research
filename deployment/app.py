a07794b3e480eb9d47f67c7af970ab43013635cd
jonathangrant <jonathanallengrant@gmail.com>
2023-02-16 19:39:06 -0500

    fixed ensemble code

--- deployment/app.py
+++ deployment/app.py
@@ -45,7 +45,7 @@ class Sequence:
         max_length = 100
         padded_ie = pad_sequences([ie], maxlen=max_length, padding='post', truncating='post')
         all_ohe = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] + [0]*(100-21))
-        return to_categorical(np.array([padded_ie[0], all_ohe]))[:1]
+        return padded_ie, to_categorical(np.array([padded_ie[0], all_ohe]))[:1]
 
 
 def residual_block(data, filters, d_rate):
@@ -96,20 +96,59 @@ def get_model():
 
     return model2
 
+def get_lstm_model():
+    x_input = Input(shape=(100,))
+    emb = Embedding(21, 128, input_length=100)(x_input)
+    bi_rnn = Bidirectional(LSTM(64, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01), bias_regularizer=l2(0.01)))(emb)
+    # bi_rnn = CuDNNLSTM(64, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01), bias_regularizer=l2(0.01))(emb)
+    x = Dropout(0.3)(bi_rnn)
 
-model = get_model()
+    # softmax classifier
+    x_output = Dense(1000, activation='softmax')(x)
+
+    model1 = Model(inputs=x_input, outputs=x_output)
+    model1.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    weights = cached_download(hf_hub_url("jonathang/Protein_Family_Models", 'model1.h5'))
+    model1.load_weights(weights)
+    return model1
+
+
+cnn_model = get_model()
+lstm_model = get_lstm_model()
+
 mappings_path = cached_download(hf_hub_url("jonathang/Protein_Family_Models", 'prot_mappings.json'))
 with open(mappings_path) as f:
     prot_mappings = json.load(f)
 
 def greet(Amino_Acid_Sequence):
-    processed_seq = Sequence.prepare(Amino_Acid_Sequence)
-    raw_prediction = model.predict(processed_seq)[0]
-    idx = raw_prediction.argmax()
+    padded_seq, processed_seq = Sequence.prepare(Amino_Acid_Sequence)
+    cnn_raw_prediction = cnn_model.predict(processed_seq)[0]
+    lstm_raw_prediction = lstm_model.predict(padded_seq)[0]
+    joined_prediction = cnn_raw_prediction*0.7 + lstm_raw_prediction*0.3
+    cnn_idx = cnn_raw_prediction.argmax()
+    lstm_idx = lstm_raw_prediction.argmax()
+    idx = joined_prediction.argmax()
+    cnn_fam_asc = prot_mappings['id2fam_asc'][str(cnn_idx)]
+    cnn_fam_id = prot_mappings['fam_asc2fam_id'][cnn_fam_asc]
+    lstm_fam_asc = prot_mappings['id2fam_asc'][str(lstm_idx)]
+    lstm_fam_id = prot_mappings['fam_asc2fam_id'][lstm_fam_asc]
     fam_asc = prot_mappings['id2fam_asc'][str(idx)]
     fam_id = prot_mappings['fam_asc2fam_id'][fam_asc]
     gc.collect()
-    return f"Input is {Amino_Acid_Sequence}.\nProcessed input is:\n{processed_seq}\n\nModel makes prediction which maps to:\nFamily Accession={fam_asc} and ID={fam_id}\n\nRaw Prediction:\n{raw_prediction}\n"
+    return f"""
+Input is {Amino_Acid_Sequence}.
+Processed input is:
+{processed_seq}
+
+CNN says: Family Accession={cnn_fam_asc} and ID={cnn_fam_id}
+LSTM says: Family Accession={lstm_fam_asc} and ID={lstm_fam_id}
+
+0.7 * cnn and 0.3 * lstm ensemble model makes prediction which maps to:
+Family Accession={fam_asc} and ID={fam_id}
+
+Raw Joined Prediction:
+{joined_prediction}
+"""
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
